a8c0d734c70aaef4594015b4a67262b8bf458fa4
Abdullah-Al-Akash <aabdullahalakash@gmail.com>
2024-07-11 01:52:30 +0600

    Dynamic Route + Load All Services from API Done

--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 const ServiceCard = ({ service }) => {
-  const { title, img, price } = service;
+  const { _id, title, img, price } = service;
   return (
     <div className="card bg-base-100 w-96 shadow-xl mx-auto">
       <figure className="px-10 pt-10n h-[200px]">
@@ -12,20 +14,22 @@ const ServiceCard = ({ service }) => {
           <h3 className="text-lg text-start font-semibold text-red-500">
             Price: ${price}
           </h3>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-8 text-red-500 rounded-full border border-red-500 hover:cursor-pointer hover:bg-red-500 hover:text-white"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
-            />
-          </svg>
+          <Link to={`/checkout/${_id}`}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-8 text-red-500 rounded-full border border-red-500 hover:cursor-pointer hover:bg-red-500 hover:text-white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </Link>
         </div>
       </div>
     </div>
